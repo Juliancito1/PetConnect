1918ed0136b607b09b2bde77dd10e40a753ccea2
JulianMartin <julianser@hotmail.com.ar>
2025-11-25 17:26:08 -0300

    Se agrego el logo en el navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router'
+import logo from '../../images/heart-pawprint-svg-file.webp'
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <section className='flex justify-around items-center bg-gray-100 py-4'>
+        <div>
+          <Link to="/" className="flex items-center space-x-2">
+            <img src={logo} alt="logo" className='w-10 h-10' />
+            <h1 className='text-2xl font-bold'>Pet Connect</h1>
+          </Link>
+        </div>
+        <div>Menu</div>
+        <div>Login</div>
+      </section>
+    </nav>
   )
 }
